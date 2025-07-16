52da943bf791bb4d49e0475b59b1796334937618
aleksandrkakovkin <aleksandrkakovkin44@gmail.com>
2025-07-17 01:05:00 +0300

    Finish

--- src/test/java/testApp/serviceTest/NotificationServiceTest.java
+++ src/test/java/testApp/serviceTest/NotificationServiceTest.java
@@ -2,25 +2,26 @@ package testApp.serviceTest;
 
 import com.icegreen.greenmail.junit5.GreenMailExtension;
 import com.icegreen.greenmail.util.ServerSetupTest;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import myApp.App;
 import myApp.service.NotificationServiceImpl;
 import myApp.userMessageKafka.UserMessageKafka;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.io.IOException;
+import java.util.Arrays;
 
-@ExtendWith(GreenMailExtension.class)
+
 @SpringBootTest(classes = App.class)
 @ActiveProfiles("test")
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class NotificationServiceTest {
     @Autowired
     private JavaMailSenderImpl javaMailSender;
@@ -28,12 +29,18 @@ class NotificationServiceTest {
     @Autowired
     private NotificationServiceImpl notificationServiceImpl;
 
-    private GreenMailExtension greenMail = new GreenMailExtension(ServerSetupTest.SMTP);
+    @RegisterExtension
+    static GreenMailExtension greenMail = new GreenMailExtension(ServerSetupTest.SMTP);
 
     @Test
-    void sendValidNotificationTest() {
+    void sendValidNotificationTest() throws MessagingException, IOException {
         UserMessageKafka userMessageKafka = new UserMessageKafka("user@example.com", "Create");
-        notificationServiceImpl.sendNotification(userMessageKafka);
+        UserMessageKafka userMessageKafkaResponse = notificationServiceImpl.sendNotification(userMessageKafka);
+        MimeMessage mimeMessage = Arrays.stream(greenMail.getReceivedMessages()).findFirst().get();
+        String content = (String)mimeMessage.getContent();
+        Assertions.assertNotNull(userMessageKafkaResponse);
+        Assertions.assertEquals("user@example.com", userMessageKafkaResponse.getEmail());
+        Assertions.assertEquals("Здравствуйте! Ваш аккаунт на сайте был успешно создан.", content);
     }
 
     @Test
