580aad8c88afd97aec0a74ef3899d2bd0f19c2b2
aleksandrkakovkin <aleksandrkakovkin44@gmail.com>
2025-07-16 18:59:50 +0300

    refactor

--- src/main/java/myApp/kafkaConsumer/KafkaConsumer.java
+++ src/main/java/myApp/kafkaConsumer/KafkaConsumer.java
@@ -1,23 +1,24 @@
-package myApp.kafkaListener;
+package myApp.kafkaConsumer;
 
 import myApp.service.NotificationService;
 import myApp.userTempKafka.UserTempKafka;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class KafkaListener {
-    private final Logger logger = LoggerFactory.getLogger(KafkaListener.class);
+public class KafkaConsumer {
+    private final Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
     private final NotificationService notificationService;
 
     @Autowired
-    public KafkaListener(NotificationService notificationService) {
+    public KafkaConsumer(NotificationService notificationService) {
         this.notificationService = notificationService;
     }
 
-    @org.springframework.kafka.annotation.KafkaListener(topics = "test-topic", groupId = "testgroup")
+    @org.springframework.kafka.annotation.KafkaListener(topics = "${app.kafka.topic}", groupId = "${app.kafka.groupId}")
     public void listen(UserTempKafka userTempKafka) {
         logger.info("Получено raw сообщение: '{}'", userTempKafka);
         notificationService.sendNotification(userTempKafka);
